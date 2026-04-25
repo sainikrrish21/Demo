da14f9930b575578e98317b3a9de926dfce84f5a
sainikrrish21 <sainikrrish21@gmail.com>
2026-04-25 12:27:00 +0530

    Delete .vscode/varlength.py

--- .vscode/varlength.py
+++
@@ -1,9 +0,0 @@
-#WAP to print this output 
-# name - 'Himanshu'
-# age - 18
-# course - 'AIDS'
-#with the help of var length argument
-def fun(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key} - {value}")
-fun(name='Himanshu', age=18 , course='AIDS') 
